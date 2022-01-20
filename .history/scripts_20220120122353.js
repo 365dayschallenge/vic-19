7599dca5efc434ef2b36f96cf5df4874c99290a5
DevLaukey <devlaukeymwaura@gmail.com>
2022-01-20 21:25:58 +0300

    the form layout set

---
+++ .history/scripts_20220120122353.js
@@ -0,0 +1,24 @@
+/* Add Randomized Reminder */
+document.getElementsByClassName(".add").click(function (event) {
+  var items = Array(
+    "Say hi to everyone in the neighborhood.",
+    "Go join the neighborhood clean up",
+    "Spend the half the day cleaning my room",
+    "Purchase pro on CodePen.io"
+  );
+
+  var item = items[Math.floor(Math.random() * items.length)];
+
+  $("ul")
+    .add(
+      "<li><a href='#'><div class='fa fa-check'></div></a> " + item + "</li>"
+    )
+    .fadeIn()
+    .appendTo("ul");
+  shuffle(item);
+});
+
+/* Closes Reminder when Check Button is clicked*/
+$("ul > li > a").click(function () {
+  $(this).parent("li").slideUp(200);
+});
